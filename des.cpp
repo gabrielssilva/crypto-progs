c035667e67cb30eccda97a1288e121be7a1aada1
Gabriel Silva <gabriel93.silva@gmail.com>
2016-04-26 12:35:36 -0300

    DES: Adds key generation function
    Signed-off-by: Gabriel Silva <gabriel93.silva@gmail.com>

--- des.cpp
+++ des.cpp
@@ -36,7 +36,56 @@ uint64_t expansion(uint32_t input)
   return output;
 }
 
-uint64_t round(uint64_t input)
+void generate_keys(uint64_t k, uint64_t *keys)
+{
+  const uint32_t MASK_28_BITS = 268435455;
+  uint64_t permuted_k = 0; // actually, only 56 bits
+
+  cout << k << endl;
+  
+  // Same permutation as expansion, only the indexes are different
+  for (int i=0; i<56; i++)
+  {
+    int new_bit_pos = DESConstants::PC1_INDEXES[i] - 1;
+    uint64_t new_bit = (k >> new_bit_pos) & 1;
+    permuted_k |= (new_bit << i);
+  }
+
+  cout << "k after PC1: " << permuted_k << endl;
+
+  // left and right slices. Only 28 bits each
+  uint32_t c, d;
+  c = (permuted_k >> 28) & MASK_28_BITS;
+  d = permuted_k & MASK_28_BITS;
+
+  cout << c << " << " << d << endl;
+
+  for (int i=0; i<16; i++)
+  {
+    int shift = DESConstants::SHIFT_SCHEDULE[i];
+    // circular shift
+    c = ((c << shift) | (c >> (28 - shift))) & MASK_28_BITS; 
+    d = ((d << shift) | (d >> (28 - shift))) & MASK_28_BITS; 
+    cout << c << " <<" << shift << "<< " << d << endl;
+
+    uint64_t ki = 0; // actually, only 48 bits
+    uint64_t c_d = c;
+    c_d = (c_d << 28) | d; // only 56 bits
+
+    // Same permutation as expansion. Again
+    for (int j=0; j<48; j++)
+    {
+      int new_bit_pos = DESConstants::PC2_INDEXES[j] - 1;
+      uint64_t new_bit = (c_d >> new_bit_pos) & 1;
+      ki |= (new_bit << j);
+    }
+
+    keys[i] = ki;
+    cout << "k" << i+1 << ": " << keys[i] << endl;
+  }  
+}
+
+uint64_t round(uint64_t input, uint64_t key)
 {
   uint32_t l_slice, r_slice;
   uint32_t *p = (uint32_t*) &input;
@@ -61,11 +110,10 @@ uint64_t round(uint64_t input)
 
 int main(int argc, char **argv)
 {
-  uint32_t in = 1;
-  cin >> in;
-  uint64_t out = expansion(in);
+  uint64_t keys[64], k;
+  cin >> k;
+  
+  generate_keys(k, keys);
 
-  cout << "in: " << in << endl;
-  cout << "out: " << out << endl;
   return 0;
 }
--- des_constants.h
+++ des_constants.h
@@ -7,6 +7,9 @@ class DESConstants
     static const int IP_INDEXES[64];
     static const int INV_IP_INDEXES[64];
     static const int E_INDEXES[48];
+    static const int PC1_INDEXES[56];
+    static const int PC2_INDEXES[48];
+    static const int SHIFT_SCHEDULE[16];
 };
 
 #endif
